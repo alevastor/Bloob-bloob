b1c92ce37d5f0e3693d8b25b14b0a8c5ac949197
alevastor <akarad@yandex.ru>
2015-01-28 10:04:41 +0200

    Added background and background scrolling

---
+++ Bloob-bloob/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed = 1;
+    public float tileSizeY = 16;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+        transform.position = new Vector3(startPosition.x, startPosition.y + newPosition, startPosition.z);
+    }
+}
