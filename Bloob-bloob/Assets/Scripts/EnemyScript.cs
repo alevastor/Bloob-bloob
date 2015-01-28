3ccd9dd1ceaf3b4eabb47050faa376390298005f
alevastor <akarad@yandex.ru>
2015-01-28 12:23:08 +0200

    Added first enemy, it's animation, spawning and movement

---
+++ Bloob-bloob/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour 
+{
+	void Start () 
+    {
+        rigidbody2D.AddForce(new Vector2((transform.position.x > 0) ? -100 : 100, 0));
+	}
+	
+	void Update () 
+    {
+	    if(transform.position.x > 4.5f || transform.position.x < -4.5f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Bloob-bloob/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour 
+{
+    public GameObject enemyObject;
+
+	void Start () 
+    {
+	    
+	}
+	
+	void Update () 
+    {
+	    if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector3 newPosition = new Vector3((Random.Range(0, 2) == 0) ? -4.5f : 4.5f, Random.Range(-6f, 6f), 0);
+            Debug.Log(Random.Range(0, 2));
+            Instantiate(enemyObject, newPosition, transform.rotation);
+        }
+	}
+}
