578bee48b48e6d368e36356f3869282f99110336
alevastor <akarad@yandex.ru>
2015-02-06 18:17:28 +0200

    Added acceleration support

--- Bloob-bloob/Assets/Scripts/AccelerationInput.cs
+++ Bloob-bloob/Assets/Scripts/AccelerationInput.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class AccelerationInput : MonoBehaviour
 {
+    float myXaccel = 0.0f;
+    float smoothSpeed = 3.0f;
+
     void Start()
     {
 
@@ -10,6 +13,13 @@ public class AccelerationInput : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Input.acceleration.x, 0, Input.acceleration.z);
+        Vector3 acceler = Input.acceleration;
+        Vector3 pos = transform.position;
+        acceler.Normalize();
+        myXaccel = Mathf.Lerp(myXaccel, acceler.x, smoothSpeed * Time.deltaTime);
+        pos.x = -myXaccel * 100f;
+        pos.y = Mathf.Abs(myXaccel) * 30f;
+        Debug.Log(myXaccel);
+        transform.position = pos;
     }
 }
